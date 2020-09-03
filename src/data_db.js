7e5cfbe091cc5b19bda27da4c96879826aeaf64b
Mutu Adi-Marian <39502043+Xxshark888xX@users.noreply.github.com>
2020-09-03 20:39:12 +0300

    Create data_db.js

---
+++ src/data_db.js
@@ -0,0 +1,802 @@
+var _DATA_ = [
+  {
+    "score": 5,
+    "date": "2018-1-5",
+    "notes": {
+      "title": "BLEENDOT",
+      "description": "esse"
+    }
+  },
+  {
+    "score": 3,
+    "date": "2018-3-10",
+    "notes": {
+      "title": "INDEXIA",
+      "description": "do"
+    }
+  },
+  {
+    "score": 3,
+    "date": "2021-12-3",
+    "notes": {
+      "title": "VIRXO",
+      "description": "sint"
+    }
+  },
+  {
+    "score": 2,
+    "date": "2018-12-10",
+    "notes": {
+      "title": "EXOSPEED",
+      "description": "nulla"
+    }
+  },
+  {
+    "score": 1,
+    "date": "2019-6-16",
+    "notes": {
+      "title": "INEAR",
+      "description": "nisi"
+    }
+  },
+  {
+    "score": 5,
+    "date": "2018-8-14",
+    "notes": {
+      "title": "BITTOR",
+      "description": "incididunt"
+    }
+  },
+  {
+    "score": 2,
+    "date": "2021-7-5",
+    "notes": {
+      "title": "OTHERSIDE",
+      "description": "reprehenderit"
+    }
+  },
+  {
+    "score": 5,
+    "date": "2021-8-17",
+    "notes": {
+      "title": "COMTRAIL",
+      "description": "ut"
+    }
+  },
+  {
+    "score": 5,
+    "date": "2020-6-24",
+    "notes": {
+      "title": "AQUOAVO",
+      "description": "elit"
+    }
+  },
+  {
+    "score": 5,
+    "date": "2019-12-24",
+    "notes": {
+      "title": "MANUFACT",
+      "description": "commodo"
+    }
+  },
+  {
+    "score": 2,
+    "date": "2019-2-7",
+    "notes": {
+      "title": "ANDERSHUN",
+      "description": "eu"
+    }
+  },
+  {
+    "score": 1,
+    "date": "2020-7-22",
+    "notes": {
+      "title": "COMVEY",
+      "description": "pariatur"
+    }
+  },
+  {
+    "score": 5,
+    "date": "2023-10-16",
+    "notes": {
+      "title": "TYPHONICA",
+      "description": "enim"
+    }
+  },
+  {
+    "score": 4,
+    "date": "2021-8-7",
+    "notes": {
+      "title": "WARETEL",
+      "description": "laborum"
+    }
+  },
+  {
+    "score": 4,
+    "date": "2022-10-10",
+    "notes": {
+      "title": "ORBEAN",
+      "description": "nulla"
+    }
+  },
+  {
+    "score": 1,
+    "date": "2019-5-5",
+    "notes": {
+      "title": "BIFLEX",
+      "description": "esse"
+    }
+  },
+  {
+    "score": 5,
+    "date": "2018-12-15",
+    "notes": {
+      "title": "GOLISTIC",
+      "description": "qui"
+    }
+  },
+  {
+    "score": 1,
+    "date": "2020-7-14",
+    "notes": {
+      "title": "SYBIXTEX",
+      "description": "cupidatat"
+    }
+  },
+  {
+    "score": 1,
+    "date": "2023-9-2",
+    "notes": {
+      "title": "COMVENE",
+      "description": "Lorem"
+    }
+  },
+  {
+    "score": 1,
+    "date": "2018-4-9",
+    "notes": {
+      "title": "IDEALIS",
+      "description": "amet"
+    }
+  },
+  {
+    "score": 3,
+    "date": "2018-12-14",
+    "notes": {
+      "title": "UNCORP",
+      "description": "proident"
+    }
+  },
+  {
+    "score": 4,
+    "date": "2022-1-25",
+    "notes": {
+      "title": "CEDWARD",
+      "description": "consequat"
+    }
+  },
+  {
+    "score": 1,
+    "date": "2018-8-18",
+    "notes": {
+      "title": "GINKLE",
+      "description": "sit"
+    }
+  },
+  {
+    "score": 1,
+    "date": "2019-9-20",
+    "notes": {
+      "title": "OBONES",
+      "description": "adipisicing"
+    }
+  },
+  {
+    "score": 2,
+    "date": "2020-5-14",
+    "notes": {
+      "title": "ENDIPINE",
+      "description": "eu"
+    }
+  },
+  {
+    "score": 3,
+    "date": "2019-3-26",
+    "notes": {
+      "title": "MIRACLIS",
+      "description": "nisi"
+    }
+  },
+  {
+    "score": 3,
+    "date": "2021-12-23",
+    "notes": {
+      "title": "OMNIGOG",
+      "description": "sit"
+    }
+  },
+  {
+    "score": 4,
+    "date": "2018-8-7",
+    "notes": {
+      "title": "OBLIQ",
+      "description": "qui"
+    }
+  },
+  {
+    "score": 5,
+    "date": "2021-12-8",
+    "notes": {
+      "title": "CAPSCREEN",
+      "description": "cupidatat"
+    }
+  },
+  {
+    "score": 1,
+    "date": "2019-3-1",
+    "notes": {
+      "title": "ZYTREX",
+      "description": "duis"
+    }
+  },
+  {
+    "score": 2,
+    "date": "2022-9-12",
+    "notes": {
+      "title": "OCTOCORE",
+      "description": "qui"
+    }
+  },
+  {
+    "score": 4,
+    "date": "2023-8-8",
+    "notes": {
+      "title": "PASTURIA",
+      "description": "excepteur"
+    }
+  },
+  {
+    "score": 4,
+    "date": "2021-6-23",
+    "notes": {
+      "title": "COMBOGENE",
+      "description": "in"
+    }
+  },
+  {
+    "score": 5,
+    "date": "2022-5-6",
+    "notes": {
+      "title": "CODAX",
+      "description": "labore"
+    }
+  },
+  {
+    "score": 4,
+    "date": "2019-6-4",
+    "notes": {
+      "title": "VITRICOMP",
+      "description": "ut"
+    }
+  },
+  {
+    "score": 1,
+    "date": "2019-8-13",
+    "notes": {
+      "title": "DIGIRANG",
+      "description": "reprehenderit"
+    }
+  },
+  {
+    "score": 4,
+    "date": "2018-11-20",
+    "notes": {
+      "title": "DAIDO",
+      "description": "mollit"
+    }
+  },
+  {
+    "score": 3,
+    "date": "2021-10-23",
+    "notes": {
+      "title": "NSPIRE",
+      "description": "laboris"
+    }
+  },
+  {
+    "score": 4,
+    "date": "2020-5-5",
+    "notes": {
+      "title": "GOKO",
+      "description": "duis"
+    }
+  },
+  {
+    "score": 2,
+    "date": "2020-3-6",
+    "notes": {
+      "title": "GAZAK",
+      "description": "esse"
+    }
+  },
+  {
+    "score": 2,
+    "date": "2023-1-6",
+    "notes": {
+      "title": "NORSUP",
+      "description": "elit"
+    }
+  },
+  {
+    "score": 4,
+    "date": "2019-6-26",
+    "notes": {
+      "title": "HELIXO",
+      "description": "cupidatat"
+    }
+  },
+  {
+    "score": 3,
+    "date": "2018-4-14",
+    "notes": {
+      "title": "CYTREX",
+      "description": "est"
+    }
+  },
+  {
+    "score": 2,
+    "date": "2021-7-9",
+    "notes": {
+      "title": "REMOLD",
+      "description": "Lorem"
+    }
+  },
+  {
+    "score": 5,
+    "date": "2023-3-3",
+    "notes": {
+      "title": "DENTREX",
+      "description": "labore"
+    }
+  },
+  {
+    "score": 2,
+    "date": "2020-9-17",
+    "notes": {
+      "title": "SURETECH",
+      "description": "labore"
+    }
+  },
+  {
+    "score": 5,
+    "date": "2021-1-3",
+    "notes": {
+      "title": "ENOMEN",
+      "description": "elit"
+    }
+  },
+  {
+    "score": 1,
+    "date": "2019-1-24",
+    "notes": {
+      "title": "EMOLTRA",
+      "description": "incididunt"
+    }
+  },
+  {
+    "score": 3,
+    "date": "2018-7-9",
+    "notes": {
+      "title": "IDETICA",
+      "description": "culpa"
+    }
+  },
+  {
+    "score": 3,
+    "date": "2022-10-8",
+    "notes": {
+      "title": "ISOLOGIX",
+      "description": "ex"
+    }
+  },
+  {
+    "score": 4,
+    "date": "2020-6-25",
+    "notes": {
+      "title": "DOGNOSIS",
+      "description": "dolor"
+    }
+  },
+  {
+    "score": 2,
+    "date": "2022-2-22",
+    "notes": {
+      "title": "EARTHPLEX",
+      "description": "nostrud"
+    }
+  },
+  {
+    "score": 2,
+    "date": "2019-2-16",
+    "notes": {
+      "title": "SKINSERVE",
+      "description": "nisi"
+    }
+  },
+  {
+    "score": 3,
+    "date": "2018-11-24",
+    "notes": {
+      "title": "ZOINAGE",
+      "description": "nostrud"
+    }
+  },
+  {
+    "score": 3,
+    "date": "2022-3-1",
+    "notes": {
+      "title": "IRACK",
+      "description": "laborum"
+    }
+  },
+  {
+    "score": 3,
+    "date": "2020-9-9",
+    "notes": {
+      "title": "GENMEX",
+      "description": "nostrud"
+    }
+  },
+  {
+    "score": 3,
+    "date": "2020-11-18",
+    "notes": {
+      "title": "PIGZART",
+      "description": "minim"
+    }
+  },
+  {
+    "score": 4,
+    "date": "2018-7-6",
+    "notes": {
+      "title": "UNIWORLD",
+      "description": "laborum"
+    }
+  },
+  {
+    "score": 2,
+    "date": "2019-8-12",
+    "notes": {
+      "title": "WAAB",
+      "description": "amet"
+    }
+  },
+  {
+    "score": 4,
+    "date": "2018-4-22",
+    "notes": {
+      "title": "CYTRAK",
+      "description": "fugiat"
+    }
+  },
+  {
+    "score": 3,
+    "date": "2022-8-1",
+    "notes": {
+      "title": "MONDICIL",
+      "description": "voluptate"
+    }
+  },
+  {
+    "score": 2,
+    "date": "2022-11-7",
+    "notes": {
+      "title": "TOURMANIA",
+      "description": "est"
+    }
+  },
+  {
+    "score": 2,
+    "date": "2023-4-10",
+    "notes": {
+      "title": "TECHTRIX",
+      "description": "irure"
+    }
+  },
+  {
+    "score": 2,
+    "date": "2020-4-1",
+    "notes": {
+      "title": "BUZZWORKS",
+      "description": "reprehenderit"
+    }
+  },
+  {
+    "score": 1,
+    "date": "2018-7-5",
+    "notes": {
+      "title": "VALPREAL",
+      "description": "aliqua"
+    }
+  },
+  {
+    "score": 5,
+    "date": "2020-4-22",
+    "notes": {
+      "title": "ENQUILITY",
+      "description": "minim"
+    }
+  },
+  {
+    "score": 1,
+    "date": "2018-4-9",
+    "notes": {
+      "title": "QUILK",
+      "description": "officia"
+    }
+  },
+  {
+    "score": 2,
+    "date": "2022-7-1",
+    "notes": {
+      "title": "SULFAX",
+      "description": "dolor"
+    }
+  },
+  {
+    "score": 3,
+    "date": "2018-6-2",
+    "notes": {
+      "title": "ACCUPRINT",
+      "description": "elit"
+    }
+  },
+  {
+    "score": 2,
+    "date": "2018-1-19",
+    "notes": {
+      "title": "DOGTOWN",
+      "description": "irure"
+    }
+  },
+  {
+    "score": 2,
+    "date": "2019-8-10",
+    "notes": {
+      "title": "ARTWORLDS",
+      "description": "pariatur"
+    }
+  },
+  {
+    "score": 1,
+    "date": "2018-8-22",
+    "notes": {
+      "title": "SEALOUD",
+      "description": "amet"
+    }
+  },
+  {
+    "score": 3,
+    "date": "2021-12-24",
+    "notes": {
+      "title": "NETERIA",
+      "description": "cillum"
+    }
+  },
+  {
+    "score": 1,
+    "date": "2019-1-22",
+    "notes": {
+      "title": "NETUR",
+      "description": "deserunt"
+    }
+  },
+  {
+    "score": 5,
+    "date": "2020-6-2",
+    "notes": {
+      "title": "XUMONK",
+      "description": "non"
+    }
+  },
+  {
+    "score": 4,
+    "date": "2018-7-13",
+    "notes": {
+      "title": "BIOLIVE",
+      "description": "pariatur"
+    }
+  },
+  {
+    "score": 1,
+    "date": "2020-10-18",
+    "notes": {
+      "title": "ZOARERE",
+      "description": "exercitation"
+    }
+  },
+  {
+    "score": 1,
+    "date": "2022-5-10",
+    "notes": {
+      "title": "BALUBA",
+      "description": "qui"
+    }
+  },
+  {
+    "score": 3,
+    "date": "2022-8-17",
+    "notes": {
+      "title": "XANIDE",
+      "description": "labore"
+    }
+  },
+  {
+    "score": 1,
+    "date": "2022-7-15",
+    "notes": {
+      "title": "PUSHCART",
+      "description": "ut"
+    }
+  },
+  {
+    "score": 1,
+    "date": "2022-5-26",
+    "notes": {
+      "title": "EXOVENT",
+      "description": "sint"
+    }
+  },
+  {
+    "score": 5,
+    "date": "2022-4-8",
+    "notes": {
+      "title": "MICROLUXE",
+      "description": "nulla"
+    }
+  },
+  {
+    "score": 1,
+    "date": "2022-5-6",
+    "notes": {
+      "title": "QNEKT",
+      "description": "pariatur"
+    }
+  },
+  {
+    "score": 5,
+    "date": "2018-7-12",
+    "notes": {
+      "title": "LYRIA",
+      "description": "velit"
+    }
+  },
+  {
+    "score": 5,
+    "date": "2021-6-14",
+    "notes": {
+      "title": "COMFIRM",
+      "description": "aliquip"
+    }
+  },
+  {
+    "score": 5,
+    "date": "2019-4-24",
+    "notes": {
+      "title": "ISOSTREAM",
+      "description": "non"
+    }
+  },
+  {
+    "score": 2,
+    "date": "2021-7-15",
+    "notes": {
+      "title": "VOLAX",
+      "description": "amet"
+    }
+  },
+  {
+    "score": 1,
+    "date": "2021-5-3",
+    "notes": {
+      "title": "ICOLOGY",
+      "description": "id"
+    }
+  },
+  {
+    "score": 3,
+    "date": "2020-6-1",
+    "notes": {
+      "title": "GEOFARM",
+      "description": "est"
+    }
+  },
+  {
+    "score": 5,
+    "date": "2019-7-7",
+    "notes": {
+      "title": "ENTHAZE",
+      "description": "non"
+    }
+  },
+  {
+    "score": 5,
+    "date": "2020-11-2",
+    "notes": {
+      "title": "IMANT",
+      "description": "tempor"
+    }
+  },
+  {
+    "score": 5,
+    "date": "2020-4-23",
+    "notes": {
+      "title": "POSHOME",
+      "description": "officia"
+    }
+  },
+  {
+    "score": 3,
+    "date": "2021-9-22",
+    "notes": {
+      "title": "BRAINCLIP",
+      "description": "qui"
+    }
+  },
+  {
+    "score": 5,
+    "date": "2019-11-17",
+    "notes": {
+      "title": "AUTOMON",
+      "description": "voluptate"
+    }
+  },
+  {
+    "score": 4,
+    "date": "2019-7-5",
+    "notes": {
+      "title": "BEADZZA",
+      "description": "voluptate"
+    }
+  },
+  {
+    "score": 4,
+    "date": "2020-8-21",
+    "notes": {
+      "title": "TUBALUM",
+      "description": "cillum"
+    }
+  },
+  {
+    "score": 4,
+    "date": "2020-5-13",
+    "notes": {
+      "title": "MIRACULA",
+      "description": "non"
+    }
+  },
+  {
+    "score": 2,
+    "date": "2018-9-23",
+    "notes": {
+      "title": "EPLOSION",
+      "description": "ea"
+    }
+  },
+  {
+    "score": 1,
+    "date": "2021-6-14",
+    "notes": {
+      "title": "NEWCUBE",
+      "description": "incididunt"
+    }
+  },
+  {
+    "score": 1,
+    "date": "2022-5-25",
+    "notes": {
+      "title": "EGYPTO",
+      "description": "eu"
+    }
+  }
+]
